9dec208502bf4a85920ab4173e28ea9f99bacf0d
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2022-08-20 14:40:20 -0700

    Chore: Update environment secrets

--- apps/client/src/utils/apolloClient.ts
+++ apps/client/src/utils/apolloClient.ts
@@ -1,8 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const APOLLO_CLIENT_URI =
-  import.meta.env.REACT_APP_APOLLO_CLIENT_URI ||
-  "http://localhost:8080/graphql";
+  import.meta.env.VITE_APOLLO_CLIENT_URL || "http://localhost:8080/graphql";
 
 const client = new ApolloClient({
   uri: APOLLO_CLIENT_URI,
