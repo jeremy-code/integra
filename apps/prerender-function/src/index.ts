d3e0df5bdd618e1d4422dbf3bab6b4c19e754de9
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2022-08-21 19:07:23 -0700

    Chore: Update function to service worker format

--- apps/prerender-function/src/index.ts
+++ apps/prerender-function/src/index.ts
@@ -1,33 +1,25 @@
 import { PRERENDERED_DOMAINS, BOT_AGENTS, IGNORE_EXTENSIONS } from "./config";
 
-export interface Env {
-  PRERENDER_API_KEY: string;
-}
+addEventListener("fetch", (event) => {
+  const { request } = event;
+  const url = new URL(request.url);
+  const { hostname } = url;
+  const requestUserAgent = (
+    request.headers.get("User-Agent") ?? ""
+  ).toLowerCase();
+  const xPrerender = request.headers.get("X-Prerender");
+  const pathName = url.pathname.toLowerCase();
+  const ext = pathName.substring(pathName.lastIndexOf(".") ?? pathName.length);
 
-export default {
-  async fetch(request: Request, env: Env): Promise<Response> {
-    const url = new URL(request.url);
-    const { hostname } = url;
-    const requestUserAgent = (
-      request.headers.get("User-Agent") ?? ""
-    ).toLowerCase();
-    const xPrerender = request.headers.get("X-Prerender");
-    const pathName = url.pathname.toLowerCase();
-    const ext = pathName.substring(
-      pathName.lastIndexOf(".") ?? pathName.length
-    );
-
-    if (
-      !xPrerender &&
-      containsOneOfThem(BOT_AGENTS, requestUserAgent) &&
-      !isOneOfThem(IGNORE_EXTENSIONS, ext) &&
-      isOneOfThem(PRERENDERED_DOMAINS, hostname)
-    ) {
-      return await prerenderRequest(request, env);
-    }
-    return new Response("Not prerendered", { status: 404 });
-  },
-};
+  if (
+    !xPrerender &&
+    containsOneOfThem(BOT_AGENTS, requestUserAgent) &&
+    !isOneOfThem(IGNORE_EXTENSIONS, ext) &&
+    isOneOfThem(PRERENDERED_DOMAINS, hostname)
+  ) {
+    event.respondWith(prerenderRequest(request));
+  }
+});
 
 /**
  * Helper function to check if an array contains an exact match for an element or not.
@@ -58,11 +50,11 @@ function containsOneOfThem(array: string[], element: string) {
  * @param {Env} env - The environment variables
  * @returns {Promise<Response>}
  */
-const prerenderRequest = async (request: Request, env: Env) => {
+const prerenderRequest = async (request: Request) => {
   const { url, headers } = request;
   const prerenderUrl = `https://service.prerender.io/${url}`;
   const headersToSend = new Headers(headers);
-  headersToSend.set("X-Prerender-Token", env.PRERENDER_API_KEY);
+  headersToSend.set("X-Prerender-Token", PRERENDER_API_KEY);
 
   const prerenderRequest = new Request(prerenderUrl, {
     headers: headersToSend,
